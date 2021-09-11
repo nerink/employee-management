7a11fd8e663eab472f561519548f562af16cfe7f
nkaur <nerinkaur2@gmail.com>
2021-09-11 16:00:59 -0400

    updated files

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
+/*Requirements
+Build a command-line application that at a minimum allows the user to:
+Add departments, roles, employees
+View departments, roles, employees
+Update employee roles
+
+Bonus points if you're able to:
+Update employee managers
+View employees by manager
+Delete departments, roles, and employees
+View the total utilized budget of a department -- ie the combined salaries of all employees in that department
+================================================*/
+
 //dependencies
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+const figlet = require("figlet");
+
+//create the connection for database
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Malaysia12!",
+    database: "employee_management_db"
+});
+connection.connect(err => {
+    if (err) throw err;
+    console.log('connection established!');
+    start();
+});
---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+DROP DATABASE IF EXISTS employee_management_db;
+CREATE DATABASE employee_management_db;
+
+USE employee_management_db;
+
+CREATE TABLE department ();
+CREATE TABLE role ();
+CREATE TABLE employee ();
