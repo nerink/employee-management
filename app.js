b379805a32f070ac9fcd8dbaf958505099ccc544
nkaur <nerinkaur2@gmail.com>
2021-09-11 16:19:31 -0400

    updated files

--- app.js
+++ app.js
@@ -15,13 +15,19 @@ View the total utilized budget of a department -- ie the combined salaries of al
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const figlet = require("figlet");
-
+//figlet application name
+figlet("Employee \n \n Manager", (err, data) => {
+    //app will throw the error and display the data
+    if (err) throw err;
+    //node app.js 
+    console.log(data);
+})
 //create the connection for database
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Malaysia12!",
+    password: "",
     database: "employee_management_db"
 });
 connection.connect(err => {
@@ -29,3 +35,7 @@ connection.connect(err => {
     console.log('connection established!');
     start();
 });
+
+// Add departments, roles, employees
+// View departments, roles, employees
+// Update employee roles
