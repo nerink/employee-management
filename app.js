19afadaa6e590d417bf4c56c452f14673850d56e
nkaur <nerinkaur2@gmail.com>
2021-09-11 22:21:09 -0400

    updated files

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Malaysia12!",
+    password: "",
     database: "employee_management_db"
 });
 connection.connect(err => {
